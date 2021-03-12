4c54d7a2de806f63dbf979d0c47837b8908357c6
Michael O'Keefe <okeefem355@gmail.com>
2021-03-12 00:17:11 -0500

    Adding config file, first e2e test, and dockerfile for app

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9-slim-buster
+
+COPY requirements.txt /tmp/
+RUN pip install -r /tmp/requirements.txt
+
+RUN mkdir -p /src
+COPY src/ /src/
+RUN pip install -e /src
+COPY tests/ /tests/
+
+WORKDIR /src
+CMD flask run --host=0.0.0.0 --port=80
---
+++ src/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_postgres_uri():
+    host = os.environ.get("DB_HOST", "localhost")
+    port = os.environ.get("POSTGRES_PORT", 5432)
+    password = os.environ.get("POSTGRES_PASSWORD", "abc123")
+    user = os.environ.get("POSTGRES_USER", "user")
+    db_name = os.environ.get("POSTGRES_DB", "ok-scoring")
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+def get_api_url():
+    host = os.environ.get("API_HOST", "localhost")
+    port = 5005 if host == "localhost" else 80
+    return f"http://{host}:{port}"
